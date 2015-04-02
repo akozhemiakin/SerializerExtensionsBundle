ca5af8441fe8f27581eb15525381965bf48c39cd
Artyom Kozhemyakin <xenus.t@gmail.com>
2015-04-02 21:43:04 +0300

    Initial commit

---
+++ src/ArkoSerializerExtensionsBundle.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * (c) Artyom Kozhemyakin <xenus.t@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Arko\SerializerExtensionsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ArkoSerializerExtensionsBundle extends Bundle {
+}
---
+++ src/DependencyInjection/SerializerExtensionsExtension.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * (c) Artyom Kozhemyakin <xenus.t@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Arko\SerializerExtensionsBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class SerializerExtensionsExtension extends Extension {
+    public function load(array $config, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load("services.yml");
+    }
+}
